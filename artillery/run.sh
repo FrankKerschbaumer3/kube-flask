11d5f8d872ec10ee3147684f692f2c622be7e083
Frankkerschbaumer3 <Frankkerschbaumer3@gmail.com>
2018-11-14 12:53:13 -0500

    Initial Commit

--- artillery/run.sh
+++ artillery/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # artillery run -e dev spp/main.yml
-DEBUG=http* ./node_modules/.bin/artillery run nginx-users.yml
+DEBUG=http* ./node_modules/.bin/artillery run nginx.user.yml
+DEBUG=http* ./node_modules/.bin/artillery run test.yml
